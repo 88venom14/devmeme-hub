5e466a1ebcc91c0482cd26f7296c814d3bfef9ba
88venom14 <artem.chernakov.07@mail.ru>
2026-05-02 05:19:07 +0300

    Deploy 2026-05-02 02:19

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -11,7 +11,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    flowType: 'pkce',
+    flowType: 'implicit',
     detectSessionInUrl: true,
   },
 });
